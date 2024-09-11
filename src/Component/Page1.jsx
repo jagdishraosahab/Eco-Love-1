aca1e8c8e268048913e65446f488433471b11ce4
darxhanx <darxhanx@gmail.com>
2024-09-11 14:52:08 +0530

    1236

--- src/Component/Page1.jsx
+++ src/Component/Page1.jsx
@@ -7,14 +7,16 @@ function Page1() {
   
   return (
     <div className="max-w-screen-xl relative select-none p-5  mx-auto">
-      <div   className="lg:mt-5 lg:relative   flex items-center justify-between">
-        <motion.h1 animate={{y:0}} initial={{y:100}} transition={{ease : "linear" , duration:0.4}} className="text-[4.9vh] lg:text-8xl font-[NewYork] uppercase text-[#838A60] ">
+      <motion.div animate={{y:0}} initial={{y:100}} transition={{ease : "linear" , duration:0.4}}  className="lg:mt-5 lg:relative lg:w-full lg:h-[13vh]    flex items-center justify-between">
+        <h1  className="text-[4.9vh] lg:text-8xl font-[NewYork] uppercase text-[#838A60] ">
           Green City Style
-        </motion.h1>
-        <Link  to="/catlog" className="  absolute top-[80%] left-[1vh] mr-2 lg:mr-0 lg:top-[9%] lg:left-[67%] lg:block lg:py-4 lg:px-28 py-2 px-32  bg-[#838A60] rounded-xl text-lg lg:text-xl font-[Poppins] text-center text-white">
+        </h1>
+
+        <Link  to="/catlog" className="  absolute top-[80%] left-[1vh] mr-2 lg:mr-0 lg:top-[9%]  lg:left-[67%] lg:block lg:py-4 lg:px-[17vh] py-2 px-32  bg-[#838A60] rounded-xl text-lg lg:text-xl font-[Poppins] text-center text-white">
           Go To Catalog
         </Link >
-      </div>
+
+      </motion.div>
       <div    className=" lg:flex  items-center justify-between">
         <motion.p animate={{y:0 , opacity:1}}  initial={{y:100 , opacity:0}} transition={{ease : "linear" , duration:0.4 , delay:0.4}}
          className="   mt-[17%] lg:mt-0 text-left text-lg font-[Poppins]  leading-6">
